3166ba6257d21e73dada7a0d83abd9ee2b6fe1ec
maks-fliuger <patinnn34@gmail.com>
2025-11-25 17:37:12 +0200

    Add files via upload

---
+++ js/formats-slider.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 16,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+
+    },
+    1280: {
+      slidesPerView: 3,
+    }
+  },
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+  },
+  navigation: {
+    nextEl: '.carousel-batton.next',
+    prevEl: '.carousel-batton.prev',
+  },
+});
